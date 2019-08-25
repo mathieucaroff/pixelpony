b184b681470bcba96d8ed05f8d8da054556d87a9
Mathieu CAROFF <mathieu.caroff@free.fr>
2019-08-25 21:47:14 +0200

    fix dev and start

--- package/script/src/serve.ts
+++ package/script/src/serve.ts
@@ -1,12 +1,20 @@
 import { server } from '@pixelpony/server'
 import { pixelPonyClientConfig } from '@pixelpony/client'
 
+import isRoot from 'is-root'
+
 let app = server({
    client: pixelPonyClientConfig,
 })
 
 let { PORT } = process.env
 
+if (PORT === undefined && isRoot()) {
+   PORT = '80'
+} else {
+   PORT = '4000'
+}
+
 app.listen(PORT, () => {
-   console.log(`🚀 Listening on port ${PORT}`)
+   console.log(`🚀 Listening on port ${PORT} || http://localhost:${PORT}`)
 })
